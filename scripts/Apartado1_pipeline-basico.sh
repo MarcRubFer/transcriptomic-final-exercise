a3ff44bdbf2d7713f70f4c275260b30cd3cb8981
MarcRubFer <rubio.fernandez.marcos@gmail.com>
2023-03-30 00:54:37 +0200

    Final version Apartado1-basic pipeline

--- scripts/Apartado1_pipeline-basico.sh
+++ scripts/Apartado1_pipeline-basico.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Requiered software
+
 # mamba install -c bioconda fastqc
 # mamba install -c bioconda hisat2
 # mamba install -c bioconda samtools
 # mamba install -c bioconda htseq
+# pip install multiqc
 
-# FastQC software - Quality of fastq files 
+# FastQC control quality
 
 mkdir -p Apartado1/output/fastqc_results
 
@@ -30,23 +32,31 @@ hisat2-build --seed 123 -p 2 \
              Apartado1/input/Homo_sapiens.GRCh38.dna.chromosome.21.fa \
              Apartado1/output/hisat2/index/Homo_sapiens.GRCh38.dna.chromosome.21 > Apartado1/output/hisat2/log/hisat2_index.log
 
-#Creación de archivo sample_id.txt
+## Make sample_id.txt
 find Apartado1/input/ -name '*.fastq' | xargs basename -s .fastq | cut -d'.' -f1 | uniq > Apartado1/output/hisat2/sample_id.txt
 
-# Align HISAT2 (On going)
-# rna-strandness default(unstranded) - desconozco la orientación del experimento
+## Align HISAT2
+    
+    # rna-strandness default(unstranded) - comment with Jaime
+ 
  for sid in $(cat Apartado1/output/hisat2/sample_id.txt); 
  do 
+        # Stablish path for fw/rv sequences
         fw_path=$(find Apartado1/input -name "*$sid*_1*")
         rv_path=$(find Apartado1/input -name "*$sid*_2*")
+        
+        # Create directory for each sample and align
         mkdir -p Apartado1/output/hisat2/results/$sid
+        
+        # Align sequences
         hisat2 --new-summary --summary-file Apartado1/output/hisat2/results/$sid/$sid.hisat2.summary \
                 --seed 123 --phred33 -p 2 -k 1 \
                 -x Apartado1/output/hisat2/index/Homo_sapiens.GRCh38.dna.chromosome.21 \
                 -1 $fw_path -2 $rv_path \
                 -S Apartado1/output/hisat2/results/$sid/$sid.sam
 
-        echo -e "\nSamtools processing: SAM -> BAM / Sorting / Indexing"
+        echo -e "\nSamtools processing: SAM -> BAM / Sorting / Indexing / Statistics"
+        # SAMTOOLS
         #1. conversion .sam to .bam
         samtools view -bS Apartado1/output/hisat2/results/$sid/$sid.sam > Apartado1/output/hisat2/results/$sid/$sid.bam
         #2. sorting
@@ -55,6 +65,11 @@ find Apartado1/input/ -name '*.fastq' | xargs basename -s .fastq | cut -d'.' -f1
         samtools index Apartado1/output/hisat2/results/$sid/$sid.sorted.bam
 
         echo -e "\n"
+
+        #SAMTOOLS alignment statistics
+        echo -e "\nSAMTOOLS alignment statistics\n" >> Apartado1/output/hisat2/results/$sid/$sid.hisat2.summary
+        samtools flagstat Apartado1/output/hisat2/results/$sid/$sid.sorted.bam >> Apartado1/output/hisat2/results/$sid/$sid.hisat2.summary
+
 
         #HTseq count
         htseq-count \
@@ -69,3 +84,9 @@ find Apartado1/input/ -name '*.fastq' | xargs basename -s .fastq | cut -d'.' -f1
             > Apartado1/output/htseq/results/"$sid".gene_counts.txt 2> Apartado1/output/htseq/log/htseq_report.log
         
  done
+
+ # MultiQC report
+
+ mkdir -p Apartado1/output/multiqc_report
+
+ multiqc -o Apartado1/output/multiqc_report/ Apartado1/output
